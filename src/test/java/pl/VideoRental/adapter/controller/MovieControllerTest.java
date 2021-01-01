ff449677472a3524b612b86ea83e6b74db523368
losu5 <losu507@tlen.pl>
2021-01-01 15:07:35 +0100

    Two methods for Get operation in MovieControllerTest. Some problems with Post operation.

--- src/test/java/pl/VideoRental/adapter/controller/MovieControllerTest.java
+++ src/test/java/pl/VideoRental/adapter/controller/MovieControllerTest.java
@@ -14,11 +14,15 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pl.VideoRental.SampleData.SampleDataStorage;
 import pl.VideoRental.domain.Movie;
+import pl.VideoRental.useCase.exception.MovieAlreadyExistException;
 import pl.VideoRental.useCase.port.moviePort.CreateMovie;
 import pl.VideoRental.useCase.port.moviePort.DeleteMovie;
 import pl.VideoRental.useCase.port.moviePort.GetAllMovies;
 import pl.VideoRental.useCase.port.moviePort.GetMovieFromCatalog;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,6 +49,21 @@ public class MovieControllerTest {
     Movie movie1 = SampleDataStorage.MOVIE_1;
     Movie movie2 = SampleDataStorage.MOVIE_2;
 
+
+    @Test
+    void shouldReturnAllMovies() throws Exception {
+        List<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+        Mockito.when(getAllMovies.getAll()).thenReturn(movies);
+        String url = "/api/movies";
+        RequestBuilder request = MockMvcRequestBuilders.get(url);
+        MvcResult mvcResult = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
+        String actualJsonResponse = mvcResult.getResponse().getContentAsString();
+        String expectedJsonResponse = objectMapper.writeValueAsString(movies);
+        assertEquals(expectedJsonResponse, actualJsonResponse);
+    }
+
     @Test
     void shouldReturnMovie() throws Exception {
         Mockito.when(getMovieFromCatalog.getByTitle(movie1.getTitle())).thenReturn(movie1);
@@ -55,5 +74,19 @@ public class MovieControllerTest {
         String expectedJsonResponse = objectMapper.writeValueAsString(movie1);
         assertEquals(expectedJsonResponse, actualJsonResponse);
     }
+
+
+/*    @Test
+    void shouldCreateNewMovie() throws Exception {
+        Mockito.when(createMovie.create(movie1)).thenReturn(movie1);
+        String url = "/api/movies/";
+       RequestBuilder request = MockMvcRequestBuilders.post(url)
+               .contentType("application/json")
+               .content(objectMapper.writeValueAsString(movie1));
+
+        MvcResult mvcResult = mockMvc.perform(request).andExpect(status().isCreated()).andReturn();
+        String actualJsonResponse =
+    }*/
+
 
 }
