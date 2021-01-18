b7f17e03f3a49a4c25ffd36c30ff44df67314e52
losu5 <losu507@tlen.pl>
2021-01-18 10:15:50 +0100

    New method in CreateDeliveryFromAnOrder class (creating Delivery from an order id instead of an order object) + new test method

--- src/main/java/pl/VideoRental/useCase/port/deliveryPort/CreateDeliveryFromAnOrder.java
+++ src/main/java/pl/VideoRental/useCase/port/deliveryPort/CreateDeliveryFromAnOrder.java
@@ -7,6 +7,8 @@ import pl.VideoRental.adapter.repository.OrderRepository;
 import pl.VideoRental.adapter.repository.UserRepository;
 import pl.VideoRental.domain.Delivery;
 import pl.VideoRental.domain.Order;
+import pl.VideoRental.useCase.exception.OrderDoesNotExistException;
+import pl.VideoRental.useCase.port.orderPort.GetOrderFromCatalog;
 
 @Component
 @RequiredArgsConstructor
@@ -14,13 +16,26 @@ public class CreateDeliveryFromAnOrder {
 
    private final OrderRepository orderRepository;
    private final DeliveryRepository deliveryRepository;
+   private final GetOrderFromCatalog getOrderFromCatalog;
    private final UserRepository userRepository;
 
-    public Delivery makeADelivery(Order order, String address ){
+    public Delivery makeDelivery(Order order, String address ){
         Delivery delivery = Delivery.builder().order(order).address(address).build();
-       // userRepository.save(order.getUser());
+       // userRepository.save(order.getUser()); - it looks that it's no necessary
         orderRepository.save(order);
         deliveryRepository.save(delivery);
         return delivery;
     }
+
+
+    public Delivery makeDelivery(long orderId, String address) {
+        try {
+           Order order = getOrderFromCatalog.getById(orderId);
+           return makeDelivery(order, address);
+        } catch (OrderDoesNotExistException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return null;
+    }
+
 }
--- src/test/java/pl/VideoRental/useCase/port/deliveryPort/CreateDeliveryFromAnOrderTest.java
+++ src/test/java/pl/VideoRental/useCase/port/deliveryPort/CreateDeliveryFromAnOrderTest.java
@@ -3,6 +3,7 @@ package pl.VideoRental.useCase.port.deliveryPort;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.VideoRental.adapter.repository.OrderRepository;
 import pl.VideoRental.domain.Copy;
 import pl.VideoRental.domain.Delivery;
 import pl.VideoRental.domain.Order;
@@ -30,6 +31,8 @@ class CreateDeliveryFromAnOrderTest {
     DeleteOrder deleteOrder;
     @Autowired
     DeleteDelivery deleteDelivery;
+    @Autowired
+    OrderRepository orderRepository;
 
     @Test
     void shouldMakeADelivery(){
@@ -46,7 +49,34 @@ class CreateDeliveryFromAnOrderTest {
                 .cost(cost)
                 .build();
         //when
-        Delivery delivery = createDeliveryFromAnOrder.makeADelivery(order, user.getAddress());
+        Delivery delivery = createDeliveryFromAnOrder.makeDelivery(order, user.getAddress());
+        //then
+        assertEquals(user.getAddress(), delivery.getAddress());
+        assertEquals(user.getId(), delivery.getOrder().getUser().getId());
+        assertEquals(copy.getId(), delivery.getOrder().getCopies().get(firstIndexNumber).getId());
+        assertFalse(delivery.isDelivered());
+        deleteOrder.deleteById(order.getId());
+        deleteDelivery.deleteById(delivery.getId());
+    }
+
+
+    @Test
+    void shouldMakeADeliveryFromOrderId(){
+        //given
+        int firstIndexNumber = 0;
+        User user = getAllUsers.getAll().get(firstIndexNumber);
+        Copy copy = getAllCopies.getAll().get(firstIndexNumber);
+        List<Copy> copies = new ArrayList<>();
+        copies.add(copy);
+        BigDecimal cost = BigDecimal.valueOf(15);
+        Order order = Order.builder()
+                .user(user)
+                .copies(copies)
+                .cost(cost)
+                .build();
+        orderRepository.save(order);
+        //when
+        Delivery delivery = createDeliveryFromAnOrder.makeDelivery(order.getId(), user.getAddress());
         //then
         assertEquals(user.getAddress(), delivery.getAddress());
         assertEquals(user.getId(), delivery.getOrder().getUser().getId());
