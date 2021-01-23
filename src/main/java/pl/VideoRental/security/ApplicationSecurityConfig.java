af85d00338f92a68f9f5050211ca84c10c41d801
losu5 <losu507@tlen.pl>
2021-01-23 12:54:03 +0100

    Spring Boot starter security was added to pom, PasswordConfig class was created, first lines in ApplicationSecurityConfig class.

--- src/main/java/pl/VideoRental/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/VideoRental/security/ApplicationSecurityConfig.java
@@ -1,4 +1,20 @@
 package pl.VideoRental.security;
 
-public class ApplicationSecurityConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final PasswordEncoder passwordEncoder;
+
+
 }
